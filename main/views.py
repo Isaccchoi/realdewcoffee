b89f4114418a3ac6467ab83646b2d44efaf99c40
isaccchoi <isaccchoi@naver.com>
2017-03-03 23:47:22 +0900

    complete ajax stuff but, reservation is not working

--- main/views.py
+++ main/views.py
@@ -1,18 +1,19 @@
 from django.conf import settings
 from django.http import Http404
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.views.generic.base import View
 from django.views.generic.edit import FormView
-from django.utils import timezone
 
-# Create your views here.
 from datetime import datetime
 from datetime import timedelta
-
+# Create your views here.
 
 from .forms import DutchOrderForm
 from .models import User
 from .models import DutchOrder
+from .models import Image
 
 
 def home(request):
@@ -44,28 +45,96 @@ def location(request):
     return render(request, 'main/location.html', ctx)
 
 
-class DutchOrderView(FormView):
-    form_class = DutchOrderForm
-    template_name = 'main/dutch_order.html'
-    print("-" * 20)
-
+class AjaxableResponseMixin(object):
+    def form_invalid(self, form):
+        response = super(AjaxableResponseMixin, self).form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        return reponse
 
     def form_valid(self, form):
-        order = form.save(commit=False)
-        print("2" * 20)
+        response = super(AjaxableResponseMixin, self).form_valid(form)
+        if self.request.is_ajax():
+            qty = self.request.GET.get("qty", 1)
+            qty = int(qty)
+            price = 12000
+            total = qty * price
+            data = {
+                'total': total,
+            }
+            print("-"*50)
+            return JsonResponse(data)
+
+        return response
+
+
+class DutchOrderView(View):
+    model = Image
+    template_name = 'main/dutch_order.html'
+
+    def post(self, request, *args, **kwargs):
         if form.is_valid():
             phone_num = form.cleaned_data.get('phone_regex')
             user, _ = User.objects.get_or_create(phone_number=phone_num)
-            print(user.id)
-            date = form.cleaned_data.get('seperate_date')
-            time = form.cleaned_data.get('seperate_time')
-            order.reserve_at = datetime.combine(date,time)
+            reserve_date = form.cleaned_data.get("seperate_date")
+            reserve_time = form.cleaned_data.get("seperate_time")
+            quantity = form.cleaned_date.get("quantity")
+            order.quantity = quantity
+            order.reserve_at = datetime.combine(reserve_date, reserve_time)
             order.user = user
             order.total_charge = order.quantity * 12000
             order.save()
             return redirect('dutch_order')
+        raise Http404 # Form이 valid 하지 않을 경우 Http404 일으킴
 
-        raise Http404
+    def get(self, request, *args, **kwargs):
+        if request.is_ajax():
+            qty = request.GET.get("qty",1)
+            try:
+                total = int(qty) * 12
+            except:
+                total = None
+            data = {
+                'total': total,
+            }
+            return JsonResponse(data)
+
+
+        form = DutchOrderForm
+        img = Image.objects.get(name="dutch")
+        ctx = {
+            'form': form,
+            'image': img,
+            'total': 12,
+        }
+
+        return render(request, "main/dutch_order.html", ctx)
+# class DutchOrderView(AjaxableResponseMixin, FormView):
+#     form_class = DutchOrderForm
+#     template_name = 'main/dutch_order.html'
+#     model = Image
+#
+#
+#     def get_queryset(self):
+#         img = Image.objects.get(name="dutch")
+#
+#
+#     def form_valid(self, form):
+#         order = form.save(commit=False)
+#         if form.is_valid():
+#             phone_num = form.cleaned_data.get('phone_regex')
+#             user, _ = User.objects.get_or_create(phone_number=phone_num)
+#             reserve_date = form.cleaned_data.get("seperate_date")
+#             reserve_time = form.cleaned_data.get("seperate_time")
+#             quantity = form.cleaned_date.get("quantity")
+#             order.quantity = quantity
+#             order.reserve_at = datetime.combine(reserve_date, reserve_time)
+#             order.user = user
+#             order.total_charge = order.quantity * 12000
+#             order.save()
+#             return redirect('dutch_order')
+#
+#         raise Http404
 
 
 #fixme
